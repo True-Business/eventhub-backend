ec177625534983f8f4132f2e2d0cf81d76705c99
mikhail <mihaelvozhzhov@gmail.com>
2025-09-20 16:40:44 +0700

    Добавляет поле isDraft при создании ивента

--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
@@ -1,12 +1,16 @@
 package ru.truebusiness.eventhub_backend.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateEventRequestDto
 import ru.truebusiness.eventhub_backend.repository.entity.Event
 import ru.truebusiness.eventhub_backend.service.model.EventModel
 
 @Mapper(componentModel = "spring")
 interface EventMapper {
+
+    @Mapping(source = "draft", target = "draft", defaultValue = "true")
     fun eventDtoToEventModel(eventRequestDto: CreateEventRequestDto): EventModel
+
     fun eventModelToEventEntity(eventModel: EventModel): Event
 }
