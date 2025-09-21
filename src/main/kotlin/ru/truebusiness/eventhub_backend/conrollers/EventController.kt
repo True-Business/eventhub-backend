ea78e7e3df0535f6b5b34346b254cb0f7741977b
mikhail <mihaelvozhzhov@gmail.com>
2025-09-21 11:34:05 +0700

    15063 Добавляет метод обновления мероприятия

--- src/main/kotlin/ru/truebusiness/eventhub_backend/conrollers/EventController.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/conrollers/EventController.kt
@@ -1,14 +1,18 @@
 package ru.truebusiness.eventhub_backend.conrollers
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.truebusiness.eventhub_backend.mapper.EventMapper
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateEventRequestDto
 import ru.truebusiness.eventhub_backend.conrollers.dto.NewEventResponse
+import ru.truebusiness.eventhub_backend.conrollers.dto.UpdateEventRequestDto
 import ru.truebusiness.eventhub_backend.service.EventService
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/v1/event")
@@ -19,6 +23,15 @@ class EventController(
     @PostMapping
     fun createEvent(@RequestBody createEventRequestDto: CreateEventRequestDto): ResponseEntity<NewEventResponse> {
         val response = eventService.createEvent(eventMapper.eventDtoToEventModel(createEventRequestDto))
+        return ResponseEntity.ok(response)
+    }
+
+    @PutMapping("/{eventID}")
+    fun updateEvent(
+        @PathVariable("eventID") eventID: UUID,
+        @RequestBody updateEventRequestDto: UpdateEventRequestDto
+    ): ResponseEntity<NewEventResponse> {
+        val response = eventService.updateEvent(eventID, eventMapper.eventDtoToEventModel(updateEventRequestDto))
         return ResponseEntity.ok(response)
     }
 }
--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
@@ -1,9 +1,12 @@
 package ru.truebusiness.eventhub_backend.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateEventRequestDto
+import ru.truebusiness.eventhub_backend.conrollers.dto.UpdateEventRequestDto
 import ru.truebusiness.eventhub_backend.repository.entity.Event
 import ru.truebusiness.eventhub_backend.service.model.EventModel
+import java.util.UUID
 
 @Mapper(componentModel = "spring")
 interface EventMapper {
@@ -11,4 +14,9 @@ interface EventMapper {
     fun eventDtoToEventModel(eventRequestDto: CreateEventRequestDto): EventModel
 
     fun eventModelToEventEntity(eventModel: EventModel): Event
+
+    fun eventDtoToEventModel(eventRequestDto: UpdateEventRequestDto): EventModel
+
+    @Mapping(source = "eventID", target = "id")
+    fun eventModelToEventEntity(eventID: UUID, eventModel: EventModel): Event
 }
