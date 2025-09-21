a0e57e599f4fac81d17e31ffc8206a91fadb2fbd
mikhail <mihaelvozhzhov@gmail.com>
2025-09-21 14:32:36 +0700

    15063 Исправляет маппинг null полей

--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/EventMapper.kt
@@ -1,12 +1,13 @@
 package ru.truebusiness.eventhub_backend.mapper
 
+import org.mapstruct.BeanMapping
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
+import org.mapstruct.MappingTarget
+import org.mapstruct.NullValuePropertyMappingStrategy
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateEventRequestDto
 import ru.truebusiness.eventhub_backend.conrollers.dto.UpdateEventRequestDto
 import ru.truebusiness.eventhub_backend.repository.entity.Event
 import ru.truebusiness.eventhub_backend.service.model.EventModel
-import java.util.UUID
 
 @Mapper(componentModel = "spring")
 interface EventMapper {
@@ -17,6 +18,6 @@ interface EventMapper {
 
     fun eventDtoToEventModel(eventRequestDto: UpdateEventRequestDto): EventModel
 
-    @Mapping(source = "eventID", target = "id")
-    fun eventModelToEventEntity(eventID: UUID, eventModel: EventModel): Event
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    fun eventModelToEventEntity(eventModel: EventModel, @MappingTarget event: Event)
 }
--- src/main/kotlin/ru/truebusiness/eventhub_backend/service/EventService.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/service/EventService.kt
@@ -3,6 +3,7 @@ package ru.truebusiness.eventhub_backend.service
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import ru.truebusiness.eventhub_backend.conrollers.dto.NewEventResponse
+import ru.truebusiness.eventhub_backend.exceptions.EventNotFoundException
 import ru.truebusiness.eventhub_backend.logger
 import ru.truebusiness.eventhub_backend.mapper.EventMapper
 import ru.truebusiness.eventhub_backend.repository.EventRepository
@@ -34,7 +35,10 @@ class EventService(
     fun updateEvent(eventID: UUID, eventModel: EventModel): NewEventResponse {
         log.info("Updating event: $eventID")
 
-        val event: Event = eventMapper.eventModelToEventEntity(eventID, eventModel)
+        val event: Event = eventRepository.findById(eventID)
+            .orElseThrow { EventNotFoundException("Event with id $eventID doesn't exist!", null) }
+        eventMapper.eventModelToEventEntity(eventModel, event)
+
         val updatedEvent = eventRepository.save(event)
 
         log.info("New event updated successfully!")
