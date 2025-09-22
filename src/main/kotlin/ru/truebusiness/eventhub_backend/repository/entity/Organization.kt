4fde932dce4f5cdcf7f41ba7b598dd7d2869103a
JsJustS <justsjs.official@gmail.com>
2025-09-22 14:41:36 +0700

    feat: automap snake_case to camelCase

--- src/main/kotlin/ru/truebusiness/eventhub_backend/repository/entity/Organization.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/repository/entity/Organization.kt
@@ -4,23 +4,25 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import java.util.*
+import java.util.UUID
 
 @Entity
 @Table(name = "organizations")
-class Organization {
+class Organization(
 
     @Id
-    var id: UUID = UUID.randomUUID()
+    var id: UUID = UUID.randomUUID(),
 
-    var name: String = ""
-    var description: String = ""
+    @Column(nullable = false, unique = true)
+    var name: String,
+    @Column(nullable = false)
+    var description: String,
 
-    var address: String? = null
-    var picture: String? = null
+    var address: String?,
+    var pictureUrl: String?,
 
-    @Column(name = "is_verified")
-    val isVerified: Boolean? = null
-    @Column(name = "creator_id")
-    val creatorId: UUID? = null
-}
+    @Column(nullable = false)
+    val isVerified: Boolean = false,
+    @Column(nullable = false)
+    val creatorId: UUID
+)
