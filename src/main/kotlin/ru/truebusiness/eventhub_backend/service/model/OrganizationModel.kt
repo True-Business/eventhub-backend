f5ef239bc3a22f6a25fdd7989b35e663c9553447
JsJustS <justsjs.official@gmail.com>
2025-09-23 17:49:03 +0700

    правка: использовал объявление класса через конструктор

--- src/main/kotlin/ru/truebusiness/eventhub_backend/service/model/OrganizationModel.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/service/model/OrganizationModel.kt
@@ -2,13 +2,13 @@ package ru.truebusiness.eventhub_backend.service.model
 
 import java.util.UUID
 
-class OrganizationModel {
-    val name: String = ""
-    val description: String = ""
+class OrganizationModel (
+    val name: String = "",
+    val description: String = "",
 
-    val address: String? = null
-    val pictureUrl: String? = null
+    val address: String?,
+    val pictureUrl: String?,
 
-    val isVerified: Boolean = false
-    var creatorId: UUID? = null
-}
+    val isVerified: Boolean = false,
+    var creatorId: UUID?
+)
