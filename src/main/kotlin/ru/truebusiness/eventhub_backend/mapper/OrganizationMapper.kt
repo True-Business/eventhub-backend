f2cabf88b7e2d802062c81dbee385e2e8bbadc56
JsJustS <justsjs.official@gmail.com>
2025-09-25 15:45:31 +0700

    правка: заглушка для маппирования creatorId -> creator

--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
@@ -1,6 +1,7 @@
 package ru.truebusiness.eventhub_backend.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateOrganizationRequestDto
 import ru.truebusiness.eventhub_backend.repository.entity.Organization
 import ru.truebusiness.eventhub_backend.service.model.OrganizationModel
@@ -8,5 +9,6 @@ import ru.truebusiness.eventhub_backend.service.model.OrganizationModel
 @Mapper(componentModel = "spring")
 interface OrganizationMapper {
     fun organizationDtoToOrganizationModel(organizationRequestDto: CreateOrganizationRequestDto): OrganizationModel
+    @Mapping(target = "creator", ignore = true)
     fun organizationModelToOrganizationEntity(organizationModel: OrganizationModel): Organization
 }
