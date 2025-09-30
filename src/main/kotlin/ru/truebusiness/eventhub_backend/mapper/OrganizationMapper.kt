b15cb733c10756d02ec549f8a77fdbec82f1753e
JsJustS <justsjs.official@gmail.com>
2025-09-30 17:35:09 +0700

    добавил маппирование creatorId -> creator

--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
@@ -2,13 +2,30 @@ package ru.truebusiness.eventhub_backend.mapper
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.springframework.beans.factory.annotation.Autowired
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateOrganizationRequestDto
+import ru.truebusiness.eventhub_backend.exceptions.UserNotFoundException
+import ru.truebusiness.eventhub_backend.repository.UserRepository
 import ru.truebusiness.eventhub_backend.repository.entity.Organization
+import ru.truebusiness.eventhub_backend.repository.entity.User
 import ru.truebusiness.eventhub_backend.service.model.OrganizationModel
+import java.util.UUID
 
 @Mapper(componentModel = "spring")
-interface OrganizationMapper {
-    fun organizationDtoToOrganizationModel(organizationRequestDto: CreateOrganizationRequestDto): OrganizationModel
-    @Mapping(target = "creator", ignore = true)
-    fun organizationModelToOrganizationEntity(organizationModel: OrganizationModel): Organization
+abstract class OrganizationMapper {
+    @Autowired
+    protected lateinit var userRepository: UserRepository
+
+    abstract fun organizationDtoToOrganizationModel(organizationRequestDto: CreateOrganizationRequestDto): OrganizationModel
+    @Mapping(target = "creator", expression = "java(mapCreatorIdToUser(organizationModel.getCreatorId()))")
+    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
+    abstract fun organizationModelToOrganizationEntity(organizationModel: OrganizationModel): Organization
+
+    protected fun mapCreatorIdToUser(creatorId: UUID): User {
+        userRepository.findUserById(creatorId)?.let {
+            return it
+        } ?: run {
+            throw UserNotFoundException("Creator not found", null)
+        }
+    }
 }
