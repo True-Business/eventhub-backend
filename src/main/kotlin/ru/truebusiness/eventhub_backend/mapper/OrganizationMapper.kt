a08d5b12315ef67b52fde8a560c0d43b378ee9da
Semyon <144766832+JsJustS@users.noreply.github.com>
2025-10-14 14:42:12 +0700

    fix: обновление бд для сохранения организации (#20)
    * Добавил колонку picture_url + снял ограничение NotNull на address
    
    * добавил маппирование creatorId -> creator
    
    * изменил поля юзера для создания пустышки в маппере

--- src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/mapper/OrganizationMapper.kt
@@ -2,13 +2,19 @@ package ru.truebusiness.eventhub_backend.mapper
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.springframework.beans.factory.annotation.Autowired
 import ru.truebusiness.eventhub_backend.conrollers.dto.CreateOrganizationRequestDto
+import ru.truebusiness.eventhub_backend.exceptions.UserNotFoundException
+import ru.truebusiness.eventhub_backend.repository.UserRepository
 import ru.truebusiness.eventhub_backend.repository.entity.Organization
+import ru.truebusiness.eventhub_backend.repository.entity.User
 import ru.truebusiness.eventhub_backend.service.model.OrganizationModel
+import java.util.UUID
 
 @Mapper(componentModel = "spring")
 interface OrganizationMapper {
     fun organizationDtoToOrganizationModel(organizationRequestDto: CreateOrganizationRequestDto): OrganizationModel
-    @Mapping(target = "creator", ignore = true)
+    @Mapping(target = "creator.id", source = "creatorId")
+    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
     fun organizationModelToOrganizationEntity(organizationModel: OrganizationModel): Organization
 }
