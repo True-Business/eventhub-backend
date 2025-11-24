f34aafb210cc1ded8b52dab0ce0e75526b838a3e
Mikhail Vozhzhov <95579334+mikhailvzhzhv@users.noreply.github.com>
2025-11-25 00:17:26 +0700

    15070 Удаление пользователя (#36)
    * 15070 Удаление пользователя

--- src/main/kotlin/ru/truebusiness/eventhub_backend/repository/OrganizationRepository.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/repository/OrganizationRepository.kt
@@ -3,10 +3,15 @@ package ru.truebusiness.eventhub_backend.repository
 import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import ru.truebusiness.eventhub_backend.repository.entity.Organization
 
 @Repository
 interface OrganizationRepository: JpaRepository<Organization, UUID>, JpaSpecificationExecutor<Organization> {
     fun existsByName(name: String): Boolean
+
+    @Query("SELECT o FROM Organization o WHERE o.creator.id = :creatorId")
+    fun findAllByCreatorId(@Param("creatorId") creatorId: UUID): List<Organization>
 }
