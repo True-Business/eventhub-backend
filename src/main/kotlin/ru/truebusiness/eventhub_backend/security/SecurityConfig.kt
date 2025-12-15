d81679e591b2ce5e34de2e1b2f592815980a4f1a
Denis Zhdanov <dinis.04@yandex.ru>
2025-12-16 05:54:57 +0700

    abxyi

--- src/main/kotlin/ru/truebusiness/eventhub_backend/security/SecurityConfig.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/security/SecurityConfig.kt
@@ -45,7 +45,7 @@ class SecurityConfig(
                         "/swagger-resources/**",
                         "/webjars/**",
                     ).permitAll()
-                    .anyRequest().permitAll()/*.authenticated()*/
+                    .anyRequest()/*.permitAll()*/.authenticated()
             }
             .httpBasic { }
             .addFilterAfter(authContextFilter, BasicAuthenticationFilter::class.java)
