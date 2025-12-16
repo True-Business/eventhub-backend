e0f02d45c3ec10e2a46128f0f7e19391deff912b
Matvey Potapov <m.potapov@g.nsu.ru>
2025-12-16 14:09:18 +0700

    Метод для проверки кредов

--- src/main/kotlin/ru/truebusiness/eventhub_backend/conrollers/users/RegistrationControllerImpl.kt
+++ src/main/kotlin/ru/truebusiness/eventhub_backend/conrollers/users/RegistrationControllerImpl.kt
@@ -4,16 +4,19 @@ import java.util.UUID
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import ru.truebusiness.eventhub_backend.conrollers.dto.UserDto
 import ru.truebusiness.eventhub_backend.conrollers.dto.users.ForgotPasswordRequest
 import ru.truebusiness.eventhub_backend.conrollers.dto.users.ConfirmForgotPasswordRequest
 import ru.truebusiness.eventhub_backend.conrollers.dto.users.RegistrationResponseDto
 import ru.truebusiness.eventhub_backend.conrollers.dto.users.UserCredentialsRegistrationDto
 import ru.truebusiness.eventhub_backend.conrollers.dto.users.UserInfoRegistrationDto
+import ru.truebusiness.eventhub_backend.mapper.UserMapper
 import ru.truebusiness.eventhub_backend.service.users.RegistrationService
 
 @RestController
 class RegistrationControllerImpl(
-    val registrationService: RegistrationService
+    val registrationService: RegistrationService,
+    private val userMapper: UserMapper,
 ) : RegistrationController {
     override fun preRegister(
         dto: UserCredentialsRegistrationDto
@@ -58,5 +61,11 @@ class RegistrationControllerImpl(
     override fun confirmForgotPassword(request: ConfirmForgotPasswordRequest): ResponseEntity<Void> {
         registrationService.confirmForgotPassword(request.code, request.password)
         return ResponseEntity(HttpStatus.OK)
+    }
+
+    override fun login(credentials: UserCredentialsRegistrationDto): ResponseEntity<UserDto> {
+        val user = userMapper.userModelToUserDto(
+            registrationService.login(credentials.email, credentials.password))
+        return ResponseEntity.ok(user)
     }
 }
